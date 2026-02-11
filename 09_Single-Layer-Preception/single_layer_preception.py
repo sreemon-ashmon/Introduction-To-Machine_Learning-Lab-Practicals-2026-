6aa38bc58bb0920f5eb2cee3dfff141ddea34070
Sree&Ash-Mon <sreeash1235@gmail.com>
2026-02-11 20:35:08 +0530

    Create single_layer_preception.py

---
+++ 09_Single-Layer-Preception/single_layer_preception.py
@@ -0,0 +1,21 @@
+import numpy as np
+from sklearn.linear_model import Perceptron
+
+X = np.array([
+    [0, 0],
+    [0, 1],
+    [1, 0],
+    [1, 1]
+])
+
+y = np.array([0, 0, 0, 1])
+
+clf = Perceptron(max_iter=1000, eta0=0.1, random_state=42)
+
+clf.fit(X, y)
+
+print("Weights:", clf.coef_)
+print("Bias:", clf.intercept_)
+
+predictions = clf.predict(X)
+print("Predictions:", predictions)
